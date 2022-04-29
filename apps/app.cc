023572338e1e3e8eaedf19488f25c2ff0b24547a
andreastedile <andrea.stedile@studenti.unitn.it>
2022-04-29 13:55:37 +0200

    Lay out basic structure

---
+++ apps/app.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "hello.h"
+
+int main() {
+  hello();
+
+  return 0;
+}
---
+++ src/hello.cc
@@ -0,0 +1,5 @@
+#include "hello.h"
+
+#include <iostream>
+
+void hello() { std::cout << "Advanced Computing Architectures project\n"; }
