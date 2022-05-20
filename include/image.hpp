3c9d2efe7dc8d583ae113d19bf60fa26d0214953
andreastedile <andrea.stedile@studenti.unitn.it>
2022-05-20 21:07:20 +0200

    Apply clang-tidy's suggestions to image

--- include/image.hpp
+++ include/image.hpp
@@ -8,35 +8,35 @@
 class Image {
   private:
     Image(std::shared_ptr<RGB[]> data, int W, int H, int row_offset, int col_offset, int w, int h);
-    std::shared_ptr<RGB[]> data;
+    std::shared_ptr<RGB[]> m_data;
     // Width of the original image
-    int W;
+    int m_W;
     // Height of the original image
-    int H;
+    int m_H;
     // X coordinate of the top-left pixel of this image, with respect to the original image
-    int row_offset;
+    int m_row_offset;
     // Y coordinate of the top-left pixel of this image, with respect to the original image
-    int col_offset;
+    int m_col_offset;
 
   public:
     // Width of this image
-    int w;
+    int m_w;
     // Width of this image
-    int h;
-    int n_pixels() const;
-    Image(const std::string& file_name);
+    int m_h;
+    [[nodiscard]] int n_pixels() const;
+    explicit Image(const std::string& file_name);
     ~Image();
     RGB& operator()(unsigned row, unsigned col);
     RGB operator()(unsigned row, unsigned col) const;
     void write_to_file(const std::string& filename) const;
-    void fill(Vec3 color);
-    Vec3 compute_sum() const;
-    Vec3 compute_sq_sum() const;
-    Vec3 compute_mean() const;
-    Vec3 compute_sq_mean() const;
-    Vec3 compute_stddev() const;
-    Image* nw();
-    Image* ne();
-    Image* se();
-    Image* sw();
+    void fill(const Vec3& color);
+    [[nodiscard]] Vec3 compute_sum() const;
+    [[nodiscard]] Vec3 compute_sq_sum() const;
+    [[nodiscard]] Vec3 compute_mean() const;
+    [[nodiscard]] Vec3 compute_sq_mean() const;
+    [[nodiscard]] Vec3 compute_stddev() const;
+    [[nodiscard]] Image* nw() const;
+    [[nodiscard]] Image* ne() const;
+    [[nodiscard]] Image* se() const;
+    [[nodiscard]] Image* sw() const;
 };
--- src/image.cpp
+++ src/image.cpp
@@ -9,82 +9,84 @@
 #include "stb_image_write.h"
 
 #include <stdexcept>
+#include <utility>
 #ifndef NDEBUG
 #include <iostream>
 #endif
 
-Image::Image(const std::string& filename) {
+// Clang-Tidy: Constructor does not initialize these fields: m_w, m_h
+Image::Image(const std::string& filename) { // NOLINT(cppcoreguidelines-pro-type-member-init)
     int n;
-    unsigned char* data = stbi_load(filename.c_str(), &w, &h, &n, 3);
+    unsigned char* data = stbi_load(filename.c_str(), &m_w, &m_h, &n, 3); // we initialize m_w, m_h here
     if (!data) {
         throw std::runtime_error("Could not open file " + filename);
         return;
     }
 
-    this->data = std::shared_ptr<RGB[]>(new RGB[w * h]);
+    m_data = std::shared_ptr<RGB[]>(new RGB[m_w * m_h]);
 
     for (int i = 0; i < n_pixels(); i++) {
-        this->data[i].R = data[i * 3 + 0];
-        this->data[i].G = data[i * 3 + 1];
-        this->data[i].B = data[i * 3 + 2];
+        m_data[i].R = data[i * 3 + 0];
+        m_data[i].G = data[i * 3 + 1];
+        m_data[i].B = data[i * 3 + 2];
     }
 
     stbi_image_free(data);
 
-    row_offset = 0;
-    col_offset = 0;
+    m_row_offset = 0;
+    m_col_offset = 0;
 
-    W = w;
-    H = h;
+    m_W = m_w;
+    m_H = m_h;
 
 #ifndef NDEBUG
-    std::cout << "width: " << w << ", height: " << h << "\n";
+    std::cout << "width: " << m_w << ", height: " << m_h << "\n";
 #endif
 }
 
 Image::Image(std::shared_ptr<RGB[]> data, int W, int H, int row_offset, int col_offset, int w, int h)
-    : data(data), W(W), H(H), row_offset(row_offset), col_offset(col_offset), w(w), h(h) {
+    : m_data(std::move(data)), m_W(W), m_H(H), m_row_offset(row_offset), m_col_offset(col_offset), m_w(w), m_h(h) {
 }
 
-Image::~Image() {
+Image::~Image() { // NOLINT(modernize-use-equals-default)
 #ifndef NDEBUG
-    std::cout << data.use_count() - 1 << "\n";
+    std::cout << m_data.use_count() - 1 << "\n";
 #endif
 }
 
 int Image::n_pixels() const {
-    return w * h;
+    return m_w * m_h;
 }
 
 inline RGB& Image::operator()(unsigned row, unsigned col) {
-    if (row >= h || col >= w)
+    if (row >= m_h || col >= m_w)
         throw std::out_of_range("Matrix subscript out of bounds");
-    return data[(row + row_offset) * W + (col_offset + col)];
+    return m_data[(row + m_row_offset) * m_W + (m_col_offset + col)];
 }
 
 inline RGB Image::operator()(unsigned row, unsigned col) const {
-    if (row >= h || col >= w)
+    if (row >= m_h || col >= m_w)
         throw std::out_of_range("const Matrix subscript out of bounds");
-    return data[(row + row_offset) * W + (col_offset + col)];
+    return m_data[(row + m_row_offset) * m_W + (m_col_offset + col)];
 }
 
 void Image::write_to_file(const std::string& filename) const {
-    unsigned char* stb_data = new unsigned char[n_pixels() * 3];
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
-            stb_data[i * w * 3 + j * 3 + 0] = operator()(i, j).R;
-            stb_data[i * w * 3 + j * 3 + 1] = operator()(i, j).G;
-            stb_data[i * w * 3 + j * 3 + 2] = operator()(i, j).B;
+    auto* stb_data = new unsigned char[n_pixels() * 3];
+    for (int i = 0; i < m_h; i++) {
+        for (int j = 0; j < m_w; j++) {
+            stb_data[i * m_w * 3 + j * 3 + 0] = operator()(i, j).R;
+            stb_data[i * m_w * 3 + j * 3 + 1] = operator()(i, j).G;
+            stb_data[i * m_w * 3 + j * 3 + 2] = operator()(i, j).B;
         }
     }
-    stbi_write_jpg(filename.c_str(), w, h, 3, stb_data, 100);
+    stbi_write_jpg(filename.c_str(), m_w, m_h, 3, stb_data, 100);
     stbi_image_free(stb_data);
 }
 
 Vec3 Image::compute_sum() const {
     Vec3 sum(0, 0, 0);
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
+    for (int i = 0; i < m_h; i++) {
+        for (int j = 0; j < m_w; j++) {
             sum.R += operator()(i, j).R;
             sum.G += operator()(i, j).G;
             sum.B += operator()(i, j).B;
@@ -95,8 +97,8 @@ Vec3 Image::compute_sum() const {
 
 Vec3 Image::compute_sq_sum() const {
     Vec3 sq_sum(0, 0, 0);
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
+    for (int i = 0; i < m_h; i++) {
+        for (int j = 0; j < m_w; j++) {
             sq_sum.R += std::pow(operator()(i, j).R, 2);
             sq_sum.G += std::pow(operator()(i, j).G, 2);
             sq_sum.B += std::pow(operator()(i, j).B, 2);
@@ -128,23 +130,23 @@ Vec3 Image::compute_stddev() const {
     return stddev;
 }
 
-void Image::fill(Vec3 color) {
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
+void Image::fill(const Vec3& color) {
+    for (int i = 0; i < m_h; i++) {
+        for (int j = 0; j < m_w; j++) {
             operator()(i, j) = color;
         }
     }
 }
 
-Image* Image::nw() {
-    return new Image(data, W, H, row_offset, col_offset, w / 2, h / 2);
+Image* Image::nw() const{
+    return new Image(m_data, m_W, m_H, m_row_offset, m_col_offset, m_w / 2, m_h / 2);
 }
-Image* Image::ne() {
-    return new Image(data, W, H, row_offset, col_offset + w / 2, w / 2, h / 2);
+Image* Image::ne() const{
+    return new Image(m_data, m_W, m_H, m_row_offset, m_col_offset + m_w / 2, m_w / 2, m_h / 2);
 }
-Image* Image::se() {
-    return new Image(data, W, H, row_offset + h / 2, col_offset + w / 2, w / 2, h / 2);
+Image* Image::se() const{
+    return new Image(m_data, m_W, m_H, m_row_offset + m_h / 2, m_col_offset + m_w / 2, m_w / 2, m_h / 2);
 }
-Image* Image::sw() {
-    return new Image(data, W, H, row_offset + h / 2, col_offset, w / 2, h / 2);
+Image* Image::sw() const{
+    return new Image(m_data, m_W, m_H, m_row_offset + m_h / 2, m_col_offset, m_w / 2, m_h / 2);
 }
