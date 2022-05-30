d4d3208a3fdc6f3b3efbd139a3011025bd675edf
andreastedile <andrea.stedile@studenti.unitn.it>
2022-05-31 00:07:51 +0200

    Automatic load balancing

--- src/quadtree.cpp
+++ src/quadtree.cpp
@@ -4,9 +4,6 @@
 
 #ifdef PARALLEL
 #include <future>
-#ifndef MAX_PARALLELISM_DEPTH
-#define MAX_PARALLELISM_DEPTH 1
-#endif
 #endif
 
 #ifndef NDEBUG
@@ -16,14 +13,14 @@
 unsigned Quadtree::n_quadtrees = 0;
 #endif
 
-Quadtree::Quadtree(const std::string &filename) : m_image(new Image(filename)), m_depth(0) {
+Quadtree::Quadtree(const std::string& filename) : m_image(new Image(filename)), m_depth(0) {
 #ifndef NDEBUG
     n_quadtrees = 1;
     id = n_quadtrees;
 #endif
 }
 
-Quadtree::Quadtree(Image *image, int depth) : m_image(image), m_depth(depth) {
+Quadtree::Quadtree(Image* image, int depth) : m_image(image), m_depth(depth) {
 #ifndef NDEBUG
     n_quadtrees++;
     id = n_quadtrees;
@@ -46,7 +43,7 @@ int Quadtree::height() const {
     return width() * height();
 }
 
-void Quadtree::write_to_file(const std::string &filename) const {
+void Quadtree::write_to_file(const std::string& filename) const {
     m_image->write_to_file(filename);
 }
 
@@ -63,31 +60,21 @@ void Quadtree::build() { // NOLINT(misc-no-recursion)
 #ifdef PARALLEL
         // Recursion: split into 4 subquadrants.
 
-        if (m_depth <= MAX_PARALLELISM_DEPTH) { // Don't overload the system.
-            // Spawn 3 threads, and assign 1 subquadrant to each of them.
-            std::future<void> ne_h = std::async(std::launch::async, &Quadtree::build, m_ne);
-            std::future<void> se_h = std::async(std::launch::async, &Quadtree::build, m_se);
-            std::future<void> sw_h = std::async(std::launch::async, &Quadtree::build, m_sw);
-            // Assign the remaining subquadrant to the current thread.
-            m_nw->build();
-            delete m_nw;
+        // Spawn 3 threads, and assign 1 subquadrant to each of them.
+        std::future<void> ne_h = std::async(std::launch::async | std::launch::deferred, &Quadtree::build, m_ne);
+        std::future<void> se_h = std::async(std::launch::async | std::launch::deferred, &Quadtree::build, m_se);
+        std::future<void> sw_h = std::async(std::launch::async | std::launch::deferred, &Quadtree::build, m_sw);
+        // Assign the remaining subquadrant to the current thread.
+        m_nw->build();
+        delete m_nw;
 
-            ne_h.get();
-            delete m_ne;
-            se_h.get();
-            delete m_se;
-            sw_h.get();
-            delete m_sw;
-        } else {
-            m_nw->build();
-            delete m_nw;
-            m_ne->build();
-            delete m_ne;
-            m_se->build();
-            delete m_se;
-            m_sw->build();
-            delete m_sw;
-        }
+        ne_h.get();
+        delete m_ne;
+        se_h.get();
+        delete m_se;
+        sw_h.get();
+        delete m_sw;
+
 #else
         m_nw->build();
         delete m_nw;
