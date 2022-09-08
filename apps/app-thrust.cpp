819810ae0f2f4613d5b1b892ed6c9cf0e7254a8b
andreastedile <andrea.stedile@studenti.unitn.it>
2022-09-08 11:17:12 +0200

    Re-enable thrust app (refactored)

--- apps/app-thrust.cpp
+++ apps/app-thrust.cpp
@@ -1,82 +1,70 @@
 #include "colorizer/colorizer.h"
-#include "file_utils.h"
+#include "quadtree/bottom_up.h"
 #include "quadtree/top_down.h"
 #include "thrust_quadrant/thrust_quadrant.cuh"
 #include "thrust_soa/thrust_soa.cuh"
 
-#if defined(TIME_READ_FILE) || defined(TIME_FLATTEN) || defined(TIME_QUADTREE_BUILD) || defined(TIME_COLORIZE) || defined(TIME_WRITE_FILE)
-#include <chrono>
-#endif
-#include <iostream>
+#include <argparse/argparse.hpp>
 #include <memory>
+#include <spdlog/spdlog.h>
+#include <spdlog/stopwatch.h>
+#include <stb_image.h>
+#include <stb_image_write.h>
 
-#define MAX_DEPTH 8
-#define DETAIL_THRESHOLD 13
+int main(int argc, char* argv[]) {
+    argparse::ArgumentParser app("app");
 
-int main(int, char* argv[]) {
-    std::cout << "Reading " << argv[1] << "...\n";
-#ifdef TIME_READ_FILE
-    auto read_start = std::chrono::steady_clock::now();
-#endif
-    int n_rows, n_cols;
-    auto* pixels = read_file(argv[1], &n_rows, &n_cols);
-#ifdef TIME_READ_FILE
-    auto read_end = std::chrono::steady_clock::now();
-    std::cout << "Read file took "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(read_end - read_start).count()
-              << " ms\n";
-#endif
+    app.add_argument("input")
+        .required()
+        .help("specify the input file");
+    app.add_argument("--top-down")
+        .default_value(false)
+        .implicit_value(true)
+        .help("specify whether to execute the top-down algorithm instead of the default bottom-up one");
+    app.add_argument("--max-depth")
+        .scan<'d', int>()
+        .default_value(8.0)
+        .help("specify the max depth");
+    app.add_argument("--detail-threshold")
+        .scan<'g', double>()
+        .default_value(13.0)
+        .help("specify the detail threshold");
 
-    std::cout << "Computing soa...\n";
-#ifdef TIME_SOA
-    auto soa_start = std::chrono::steady_clock::now();
-#endif
+    app.parse_args(argc, argv);
+
+    auto input = app.get("input");
+    auto max_depth = app.get<int>("--max-depth");
+    auto detail_threshold = app.get<double>("--detail-threshold");
+
+    spdlog::stopwatch sw;
+
+    spdlog::info("Read {}", argv[1]);
+    int n_rows, n_cols, n;
+    uint8_t* pixels = stbi_load(input.c_str(), &n_cols, &n_rows, &n, 3);
+
+    spdlog::info("Compute SoA");
+    sw.reset();
     const auto soa = to_thrust_pixel_soa(pixels, n_rows * n_cols);
-#ifdef TIME_SOA
-    auto soa_end = std::chrono::steady_clock::now();
-    std::cout << "Soa took "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(soa_end - soa_start).count()
-              << " ms\n";
-#endif
+    spdlog::info("Compute SoA took {} ms", std::chrono::duration_cast<std::chrono::milliseconds>(sw.elapsed()).count());
 
-    std::cout << "Building quadtree...\n";
-#ifdef TIME_QUADTREE_BUILD
-    auto build_start = std::chrono::steady_clock::now();
-#endif
+    spdlog::info("Build quadtree top down");
+    sw.reset();
     auto quadrant = std::make_unique<ThrustQuadrant>(0, 0, n_rows, n_cols, soa);
-    auto root = top_down(std::move(quadrant), DETAIL_THRESHOLD, MAX_DEPTH);
-#ifdef TIME_QUADTREE_BUILD
-    auto build_end = std::chrono::steady_clock::now();
-    std::cout << "Quadtree build took "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(build_end - build_start).count()
-              << " ms\n";
-#endif
+    auto root = top_down(std::move(quadrant), detail_threshold, max_depth);
 
-    std::cout << "Coloring...\n";
-#ifdef TIME_COLORIZE
-    auto colorize_start = std::chrono::steady_clock::now();
-#endif
+    spdlog::info("Build quadtree took {} ms", std::chrono::duration_cast<std::chrono::milliseconds>(sw.elapsed()).count());
+
+    spdlog::info("Colorize");
+    sw.reset();
     colorize(pixels, n_rows, n_cols, *root);
-#ifdef TIME_COLORIZE
-    auto colorize_end = std::chrono::steady_clock::now();
-    std::cout << "Colorize took "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(colorize_end - colorize_start).count()
-              << " ms\n";
-#endif
+    spdlog::info("Colorize took {} ms", std::chrono::duration_cast<std::chrono::milliseconds>(sw.elapsed()).count());
 
-    std::cout << "Writing result...\n";
-#ifdef TIME_WRITE_FILE
-    auto write_start = std::chrono::steady_clock::now();
-#endif
-    write_file("result.jpg", pixels, n_rows, n_cols);
-#ifdef TIME_WRITE_FILE
-    auto write_end = std::chrono::steady_clock::now();
-    std::cout << "Write took "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(write_end - write_start).count()
-              << " ms\n";
-#endif
+    spdlog::info("Write image");
+    stbi_write_jpg("result.jpg", n_cols, n_rows, 3, pixels, 100);
 
-    stbi_image_free(pixels);
+    delete[] pixels;
+
+    spdlog::info("All done");
 
     return 0;
 }
