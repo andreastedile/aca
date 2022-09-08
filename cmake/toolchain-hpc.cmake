85d1b1932b3fb14ef45ea1e0d70c2c20559a6b73
andreastedile <andrea.stedile@studenti.unitn.it>
2022-09-08 11:54:38 +0200

    Removed app-thrust and sources

--- cmake/toolchain-hpc.cmake
+++ cmake/toolchain-hpc.cmake
@@ -1,3 +1,2 @@
 set(CMAKE_C_COMPILER /apps/gcc-9.1.0/local/bin/gcc-9.1.0)
 set(CMAKE_CXX_COMPILER /apps/gcc-9.1.0/local/bin/g++-9.1.0)
-set(CMAKE_CUDA_HOST_COMPILER /apps/gcc-9.1.0/local/bin/g++-9.1.0)
