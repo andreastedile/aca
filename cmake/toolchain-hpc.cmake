31e1cedad9c4f24ca9d38de3fb3a5995339da1a8
andreastedile <andrea.stedile@studenti.unitn.it>
2022-09-08 11:28:24 +0200

    Add hpc toolchain file

---
+++ cmake/toolchain-hpc.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_C_COMPILER /apps/gcc-9.1.0/local/bin/gcc-9.1.0)
+set(CMAKE_CXX_COMPILER /apps/gcc-9.1.0/local/bin/g++-9.1.0)
+set(CMAKE_CUDA_HOST_COMPILER /apps/gcc-9.1.0/local/bin/g++-9.1.0)
