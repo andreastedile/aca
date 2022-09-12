9df003737158c6cc5280fb24aea6aaa1aedac804
Gabriele Masina <gabriele.masina@virgilio.it>
2022-09-12 09:39:44 +0200

    Add cpu iter app

--- apps/iterative/qt_it_cpu.cu
+++ apps/iterative/qt_it_cpu.cu
@@ -1,7 +1,5 @@
 #include "colorization.h"
-#include "construction_device.cuh"
 #include "construction_host.h"
-#include "cuda_check.h"
 #include "node.h"
 #include "padding.h"
 #include "qtmath.h"
@@ -9,7 +7,6 @@
 
 #include <argparse/argparse.hpp>
 #include <chrono>
-#include <cuda_runtime.h>
 #include <fstream>
 #include <omp.h>
 #include <spdlog/spdlog.h>
@@ -22,7 +19,7 @@ using ms = std::chrono::milliseconds;
 int main(int argc, char* argv[]) {
     spdlog::set_level(spdlog::level::debug);
 
-    argparse::ArgumentParser app("qt-it-gpu");
+    argparse::ArgumentParser app("qt-it-cpu");
 
     app.add_argument("--input")
         .required()
@@ -31,14 +28,6 @@ int main(int argc, char* argv[]) {
         .scan<'g', float>()
         .default_value(13.0f)
         .help("specify the detail threshold");
-    app.add_argument("-b", "--blocks")
-        .scan<'d', int>()
-        .default_value(1024)
-        .help("specify the number of blocks in the kernel launch");
-    app.add_argument("-t", "--threads")
-        .scan<'d', int>()
-        .default_value(256)
-        .help("specify the number of thread per blocks in the kernel launch");
     app.add_argument("--no-output-file")
         .default_value(false)
         .implicit_value(true)
@@ -52,8 +41,6 @@ int main(int argc, char* argv[]) {
 
     auto input = app.get("--input");
     auto detail_threshold = app.get<float>("--detail-threshold");
-    auto n_blocks = app.get<int>("--blocks");
-    auto n_threads = app.get<int>("--threads");
     auto no_output_file = app.get<bool>("--no-output-file");
     auto csv = app.get<bool>("--csv");
 
@@ -81,59 +68,30 @@ int main(int argc, char* argv[]) {
 
     int tree_height = log4(n_pixels);
     int n_nodes = (pow_base4(tree_height + 1) - 1) / 3;
-    int n_leaves_per_thread = n_pixels / (n_blocks * n_threads);
-
-    spdlog::info("Allocate RGB AoS and quadtree array on device");
-    auto allocate_device_start = Clock::now();
-    RGB<unsigned char>* d_aos_aos;
-    Node* d_quadtree_array;
-    CHECK(cudaMalloc(&d_aos_aos, n_pixels * sizeof(RGB<unsigned char>)));
-    CHECK(cudaMalloc(&d_quadtree_array, n_nodes * sizeof(Node)));
-    RGBAoS d_aos(d_aos_aos, n_pixels);
-    auto allocate_device_end = Clock::now();
-
-    spdlog::info("Copy RGB AoS on the device");
-    auto copy_to_device_start = Clock::now();
-    CHECK(cudaMemcpy(d_aos_aos, h_aos.aos, n_pixels * sizeof(RGB<unsigned char>), cudaMemcpyHostToDevice));
-    auto copy_to_device_end = Clock::now();
-
-    delete[] h_aos.aos;
-
-    spdlog::info("Construct quadtree array on device (launch: {}/{}, n leaves per thread: {})", n_blocks, n_threads, n_leaves_per_thread);
-    auto construct_device_start = Clock::now();
-    construct_quadtree_array_device<<<n_blocks, n_threads>>>(d_quadtree_array, tree_height, d_aos, detail_threshold, n_leaves_per_thread);
-    CHECK(cudaPeekAtLastError());
-    CHECK(cudaDeviceSynchronize());
-    auto construct_device_end = Clock::now();
 
     spdlog::info("Allocate quadtree array on host (size in bytes: {})", (size_t)n_nodes * (sizeof(Node)));
     auto allocate_on_host_start = Clock::now();
     Node* h_quadtree_array = new Node[n_nodes];
     auto allocate_on_host_end = Clock::now();
 
-    spdlog::info("Copying quadtree array back to host");
-    auto copy_to_host_start = Clock::now();
-    CHECK(cudaMemcpy(h_quadtree_array, d_quadtree_array, n_nodes * sizeof(Node), cudaMemcpyDeviceToHost));
-    auto copy_to_host_end = Clock::now();
-
-    CHECK(cudaFree(d_aos_aos));
-    CHECK(cudaFree(d_quadtree_array));
-
-    int from_level = log4(n_blocks) - 1;
-    spdlog::info("Construct quadtree array on host from level {}", from_level);
     omp_set_num_threads(8);
+    spdlog::info("Init leaves on host");
+    auto init_leaves_on_host_start = Clock::now();
+    init_quadtree_array_leaves_host(h_quadtree_array, tree_height, h_aos);
+    auto init_leaves_on_host_end = Clock::now();
+
+    int from_level = tree_height - 1;
+    spdlog::info("Construct quadtree array on host from level {}", from_level);
     auto construct_on_host_start = Clock::now();
     construct_quadtree_array_host(h_quadtree_array, from_level, detail_threshold);
     auto construct_on_host_end = Clock::now();
 
     if (csv) {
         std::ofstream file("timings.csv");
-        file << "flatten_ms, allocate_device_ms, copy_to_device_ms, construct_on_device_ms, allocate_on_host_ms, construct_on_host_ms\n";
+        file << "flatten_ms, allocate_on_host_ms, init_leaves_host, construct_on_host_ms\n";
         file << std::chrono::duration_cast<ms>(flatten_end - flatten_start).count() << ", ";
-        file << std::chrono::duration_cast<ms>(allocate_device_end - allocate_device_start).count() << ", ";
-        file << std::chrono::duration_cast<ms>(copy_to_device_end - copy_to_host_start).count() << ", ";
-        file << std::chrono::duration_cast<ms>(construct_device_end - construct_device_start).count() << ", ";
         file << std::chrono::duration_cast<ms>(allocate_on_host_end - allocate_on_host_start).count() << ", ";
+        file << std::chrono::duration_cast<ms>(init_leaves_on_host_end - init_leaves_on_host_start).count() << ", ";
         file << std::chrono::duration_cast<ms>(construct_on_host_end - construct_on_host_start).count() << '\n';
     }
 
@@ -147,6 +105,8 @@ int main(int argc, char* argv[]) {
             stbi_write_jpg(output.c_str(), n_cols, n_rows, 3, pixels, 100);
         }
     }
+
+    delete[] h_aos.aos;
 
     delete[] h_quadtree_array;
     delete[] pixels;
